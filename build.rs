c1cf61c029d0186ba399742f1526e0a608c542b8
lcxl <lcx87654321@163.com>
2026-03-16 02:20:21 +0800

    feat: upgrade version to 0.6.0 and support prebuilt x264 on windows

--- build.rs
+++ build.rs
@@ -1,15 +1,19 @@
 extern crate pkg_config;
 
 fn main() {
-    let x264 = pkg_config::Config::new()
-        .cargo_metadata(false)
-        .probe("x264")
-        .expect("Is x264 installed?");
+    let version = std::env::var("DEP_X264_VERSION").ok().or_else(|| {
+        pkg_config::Config::new()
+            .cargo_metadata(false)
+            .probe("x264")
+            .ok()
+            .map(|x264| x264.version)
+    });
 
-    let n: Option<u64> = x264.version
-        .split('.')
-        .nth(1)
-        .and_then(|n| n.parse().ok());
+    let n: Option<u64> = version.as_ref().and_then(|v| {
+        v.split('.')
+            .nth(1)
+            .and_then(|n| n.parse().ok())
+    });
 
     if let Some(n) = n {
         if n >= 149 {
--- src/data.rs
+++ src/data.rs
@@ -107,11 +107,11 @@ impl<'a> AsRef<[u8]> for Unit<'a> {
 /// The importance of a given unit.
 pub enum Priority {
     /// Not important at all.
-    Disposable = nal_priority_e::NAL_PRIORITY_DISPOSABLE as i32,
+    Disposable = nal_priority_e_NAL_PRIORITY_DISPOSABLE as i32,
     /// Not very important.
-    Low = nal_priority_e::NAL_PRIORITY_LOW as i32,
+    Low = nal_priority_e_NAL_PRIORITY_LOW as i32,
     /// Pretty important.
-    High = nal_priority_e::NAL_PRIORITY_HIGH as i32,
+    High = nal_priority_e_NAL_PRIORITY_HIGH as i32,
     /// Extremely important.
-    Highest = nal_priority_e::NAL_PRIORITY_HIGHEST as i32,
+    Highest = nal_priority_e_NAL_PRIORITY_HIGHEST as i32,
 }
